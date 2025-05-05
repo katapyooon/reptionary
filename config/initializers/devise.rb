497c1582657c621e763b59c3c8f35a5c1834084c
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-05-06 00:12:03 +0900

    微修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,5 +2,5 @@ Devise.setup do |config|
     # Other configurations...
 
     # Add a pepper to devise's password hashing
-    config.pepper = ”a_secure_random_string_here”
+    config.pepper = "a_secure_random_string_here"
 end
