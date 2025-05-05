09213b12ae4c76d7c676b7819056066e5188c33a
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-05-05 23:27:36 +0900

    CIが落ちてたので、db:migrate

--- db/migrate/20250505135229_create_users.rb
+++ db/migrate/20250505135229_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
-    create_table :users do |t|
+    # テーブルが存在しない場合のみ作成
+    create_table :users, if_not_exists: true do |t|
       t.string :name
       t.string :email
       t.string :password_digest
