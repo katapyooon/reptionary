e4395a5c138f8cdae0f533020bcfb3bd4e007771
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-05-05 23:49:03 +0900

    CI落ちたので再修正

--- db/migrate/20250505135229_create_users.rb
+++ db/migrate/20250505135229_create_users.rb
@@ -4,9 +4,15 @@ class CreateUsers < ActiveRecord::Migration[7.2]
     create_table :users, if_not_exists: true do |t|
       t.string :name
       t.string :email
-      t.string :password_digest
+      t.string :encrypted_password, default: "", null: false
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.datetime :remember_created_at
 
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
+    add_index :users, :reset_password_token, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_05_135229) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "name"
+    t.string "email"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
