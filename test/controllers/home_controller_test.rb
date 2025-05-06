25154df6fdb87320dd796e1f268cd7475caadb86
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-05-06 15:41:00 +0900

    CI落ちたので再修正

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers # Devise のテストヘルパーをインクルード
+
+  def setup
+    @user = users(:one) # テスト用のユーザーをフィクスチャから取得
+    sign_in @user       # ユーザーをログイン
+  end
+
   test "should get index" do
     get home_index_url
     assert_response :success
