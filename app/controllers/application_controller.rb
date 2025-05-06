29c14f4bf8a3529dbe2d6909b466de19e82c13a5
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-05-06 15:31:19 +0900

    ログイン・ログアウトの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,29 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :authenticate_user! # 全てのページでログインを必須にする
+
+  def after_sign_in_path_for(resource)
+    home_index_path # ログイン後のリダイレクト先を設定
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path # ログアウト後にログイン画面に遷移
+  end
+
+  def manifest
+    render json: {
+      name: "My App",
+      short_name: "App",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#ffffff",
+      theme_color: "#000000",
+      icons: [
+        {
+          src: "/icon.png",
+          sizes: "192x192",
+          type: "image/png"
+        }
+      ]
+    }
+  end
 end
