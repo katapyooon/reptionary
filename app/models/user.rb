762e4fa593d70ae34888975ec7cb4c5df2d86d86
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-05-11 15:48:07 +0900

    MyReptilesのページに正しく遷移されるよう修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # ユーザーは複数の爬虫類を持つ
+  has_many :my_reptiles, dependent: :destroy
 end
