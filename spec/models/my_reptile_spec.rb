99bc0b0e5cd48a38318ca80b86a821abfc049e55
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-05-27 00:13:05 +0900

    CI落ちたので再々修正

--- spec/models/my_reptile_spec.rb
+++ spec/models/my_reptile_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe MyReptile, type: :model do
-  let(:user) { User.create!(email: "test@example.com", password: "password") }
-
   it "is valid with valid attributes" do
+    user = create(:user)
     my_reptile = MyReptile.new(
       name: "Gecko",
       description: "A small lizard",
@@ -11,7 +10,7 @@ RSpec.describe MyReptile, type: :model do
       adoption_date: "2025-05-22",
       gender: "male",
       image: Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/files/sample_image.jpg"), "image/jpeg"),
-      user: create(:user)
+      user: user
     )
     expect(my_reptile).to be_valid
   end
