e3734a4ba3554fc4930894449acdf0524f433034
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-06-14 18:39:22 +0900

    rspec落ちたので、spec周りを修正

--- spec/helpers/wishlist_reptiles_helper_spec.rb
+++ spec/helpers/wishlist_reptiles_helper_spec.rb
@@ -1,15 +1,52 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the WishlistReptilesHelper. For example:
-#
-# describe WishlistReptilesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-RSpec.describe WishlistReptilesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "WishlistReptiles", type: :request do
+  let(:user) { create(:user) }
+  
+  before do
+    sign_in user
+  end
+
+  describe "GET /index" do
+    it "returns http success" do
+      get user_wishlist_reptiles_path(user)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    let(:wishlist_reptile) { create(:wishlist_reptile, user: user) }
+    
+    it "returns http success" do
+      get user_wishlist_reptile_path(user, wishlist_reptile)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /new" do
+    it "returns http success" do
+      get new_user_wishlist_reptile_path(user)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST /create" do
+    let(:valid_attributes) { attributes_for(:wishlist_reptile) }
+
+    it "creates a new wishlist reptile" do
+      expect {
+        post user_wishlist_reptiles_path(user), params: { wishlist_reptile: valid_attributes }
+      }.to change(WishlistReptile, :count).by(1)
+      expect(response).to redirect_to(user_wishlist_reptiles_path(user))
+    end
+  end
+
+  describe "GET /edit" do
+    let(:wishlist_reptile) { create(:wishlist_reptile, user: user) }
+
+    it "returns http success" do
+      get edit_user_wishlist_reptile_path(user, wishlist_reptile)
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
