bc28bca049e98cd592d3fa2597aa10da4f96c132
片所宏介 <katashokousuke@katashokousukenoMacBook-Air.local>
2025-06-18 16:29:45 +0900

    starアイコンを押下したらwidhlist_reptilesのページに遷移するように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   # ユーザーは複数の爬虫類を持つ
   has_many :my_reptiles, dependent: :destroy
+  # ユーザーは複数のウィッシュリスト爬虫類を持つ
+  has_many :wishlist_reptiles, dependent: :destroy
 end
